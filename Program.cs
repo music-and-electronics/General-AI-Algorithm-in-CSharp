808361c970147983bf9bc9ed1ebdfeac31b35cad
musicandelectronics <kjh970407@naver.com>
2019-05-21 01:24:13 +0900

    NEW

--- Program.cs
+++ Program.cs
@@ -151,7 +151,7 @@ namespace Ai_hw_8
         public ANN()
         {
             Console.Write("Input the level of hidden layer: ");
-            Console.WriteLine("This is a sample for Git hub TEST!");
+            Console.WriteLine("This is a sample for Git hub TEST! Again!");
             int layer_num = Int32.Parse(Console.ReadLine())+2;
             Initial_DATA initial = new Initial_DATA(layer_num);
 
